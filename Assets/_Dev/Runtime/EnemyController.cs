1a5c164a3ca04edb54df5812580e27e45464d4a6
Sorra the Orc <sorra@thewizardscode.com>
2023-12-17 16:49:13 -0800

    Create the first enemy. No real behaviours yet, but the enemy exists. Also add a bunch of Kenney CC0 prototype textures

---
+++ Assets/_Dev/Runtime/EnemyController.cs
@@ -0,0 +1,35 @@
+using NeoFPS;
+using NeoFPS.SinglePlayer;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Playground.December23
+{
+    public class EnemyController : MonoBehaviour
+    {
+        [SerializeField, Tooltip("How fast the enemy moves.")]
+        private float speed = 5f;
+        [SerializeField, Tooltip("How fast the enemy rotates.")]
+        private float rotationSpeed = 1f;
+
+        private void Update()
+        {
+            if (FpsSoloCharacter.localPlayerCharacter == null)
+                return;
+
+            Vector3 destination = FpsSoloCharacter.localPlayerCharacter.localTransform.position;
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+
+            Vector3 directionToTarget = destination - transform.position;
+            directionToTarget.y = 0;
+            if (directionToTarget != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(directionToTarget);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            }
+        }
+
+
+    }
+}
