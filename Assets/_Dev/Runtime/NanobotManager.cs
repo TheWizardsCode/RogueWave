d9efbcae9fde4de324267662b185c1fc2152c32a
Sorra the Orc <sorra@thewizardscode.com>
2023-12-19 00:07:30 -0800

    Add current resources to the UI

--- Assets/_Dev/Runtime/NanobotManager.cs
+++ Assets/_Dev/Runtime/NanobotManager.cs
@@ -1,17 +1,34 @@
+using NeoFPS;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static NeoFPS.BasicHealthManager;
 
 namespace Playground
 {
     public class NanobotManager : MonoBehaviour
     {
+        public delegate void OnResourcesChanged(float from, float to);
+        public event OnResourcesChanged onResourcesChanged;
+
+        private int currentResources = 0;
+
         /// <summary>
         /// The amount of resources the player currently has.
         /// </summary>
         public int resources
         {
-            get; set;
+            get { return currentResources; }
+            set
+            {
+                if (currentResources == value)
+                    return;
+
+                if (onResourcesChanged != null)
+                    onResourcesChanged(currentResources, value);
+
+                currentResources = value;
+            }
         }
     }
 }
---
+++ Assets/_Dev/Runtime/UI/HudResourceCounter.cs
@@ -0,0 +1,48 @@
+﻿using NeoFPS;
+using UnityEngine;
+using UnityEngine.UI;
+using static NeoFPS.HealthDelegates;
+
+namespace Playground
+{
+	public class HudResourceCounter : PlayerCharacterHudBase
+    {
+		[SerializeField, Tooltip("The text readout for the current characters resources.")]
+		private Text m_ResourcesText = null;
+
+        private NanobotManager nanobotManager = null;
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            if (nanobotManager != null)
+                nanobotManager.onResourcesChanged -= OnResourcesChanged;
+        }
+
+        public override void OnPlayerCharacterChanged(ICharacter character)
+        {
+			if (nanobotManager != null)
+                nanobotManager.onResourcesChanged -= OnResourcesChanged;
+
+            if (character as Component != null)
+                nanobotManager = character.GetComponent<NanobotManager>();
+            else
+                nanobotManager = null;
+
+            if (nanobotManager != null)
+			{
+                nanobotManager.onResourcesChanged += OnResourcesChanged;
+				OnResourcesChanged(0f, nanobotManager.resources);
+				gameObject.SetActive (true);
+			}
+			else
+                gameObject.SetActive (false);
+		}
+
+		protected virtual void OnResourcesChanged (float from, float to)
+        {
+            m_ResourcesText.text = ((int)to).ToString ();
+		}
+	}
+}
