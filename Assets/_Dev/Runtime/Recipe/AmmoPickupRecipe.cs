359da8458d4fd80f0d0459d8b237fdc57d7b3953
Sorra the Orc <sorra@thewizardscode.com>
2023-12-19 23:04:44 -0800

    Make the nanobot recipe system extensible and add a health recipe

---
+++ Assets/_Dev/Runtime/Recipe/AmmoPickupRecipe.cs
@@ -0,0 +1,42 @@
+﻿using NeoFPS;
+using NeoFPS.ModularFirearms;
+using NeoFPS.SinglePlayer;
+using System;
+using UnityEngine;
+
+namespace Playground
+{
+    [CreateAssetMenu(fileName = "Ammo Pickup Recipe", menuName = "Playground/Ammo Pickup Recipe")]
+    public class AmmoPickupRecipe : Recipe<Pickup>
+    {
+        [SerializeField, Tooltip("The ammo type this recipe creates.")]
+        private SharedAmmoType ammo;
+
+        private FpsInventorySwappable _inventory;
+        private FpsInventorySwappable inventory
+        {
+            get
+            {
+                if (_inventory == null)
+                {
+                    _inventory = FpsSoloCharacter.localPlayerCharacter.inventory as FpsInventorySwappable;
+                }
+                return _inventory;
+            }
+        }
+        [NonSerialized] private InventoryItemPickup inventoryPickup;
+
+        public override bool ShouldBuild
+        {
+            get
+            {
+                SharedPoolAmmo sharedPoolAmmo = inventory.selected.GetComponent<SharedPoolAmmo>();
+                if (sharedPoolAmmo == null)
+                {
+                    return false;
+                }
+                return sharedPoolAmmo.ammoType.itemIdentifier == ammo.itemIdentifier;
+            }
+        }
+    }
+}
