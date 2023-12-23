69ef933b541ce3d853e43b19a1b34703dee0018b
Yondernauts Games <47190251+YondernautsGames@users.noreply.github.com>
2023-12-23 22:27:38 +0000

    Movement upgrade system, better victory handling, tweaked pickups
    Added MovementUpgradeSystem to character and hooked in to motion graph;
    Modified pickups to remove solid colliders causing character collision issues;
    Added new victory popup;
    Modified game mode to show victory condition popup;

---
+++ Assets/_Dev/Runtime/UI/HudVictoryPopup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NeoFPS.SinglePlayer;
+using NeoFPS;
+using System;
+
+namespace Playground
+{
+    [RequireComponent (typeof (CanvasGroup))]
+	public class HudVictoryPopup : MonoBehaviour
+    {
+        private CanvasGroup m_CanvasGroup = null;
+
+        void Awake()
+        {
+            m_CanvasGroup = GetComponent<CanvasGroup>();
+            PlaygroundDecember23GameMode.onVictory += OnVictory;
+            m_CanvasGroup.alpha = 0f;
+            gameObject.SetActive(false);
+        }
+
+        private void OnVictory()
+        {
+            m_CanvasGroup.alpha = 1f;
+            gameObject.SetActive(true);
+        }
+    }
+}
--- Assets/_Dev/Runtime/Upgrades/MovementUpgradeManager.cs
+++ Assets/_Dev/Runtime/Upgrades/MovementUpgradeManager.cs
@@ -11,7 +11,147 @@ namespace Playground
     [RequireComponent(typeof(MotionController))]
     public class MovementUpgradeManager : MonoBehaviour, IMotionGraphDataOverride
     {
+        #region MOTION DATA MODIFIERS
+
+        /*
+        
+        FLOAT -
+        - moveSpeedWalking
+        - moveSpeedWalkAiming
+        - moveSpeedSprinting
+        - moveSpeedSprintAiming
+        - moveSpeedCrouching
+        - moveSpeedAirWalk
+        - moveSpeedAirSprint
+        - moveSpeedAirCrouch
+        - acceleration
+        - accelerationAirborne
+        - deceleration
+        - maxJumpHeight
+        - jetpackForce
+        - dashSpeed
+
+        */
+
         public FloatValueModifier moveSpeed { get; private set; }
+        public FloatValueModifier moveSpeedAirborne { get; private set; }
+        public FloatValueModifier acceleration { get; private set; }
+        public FloatValueModifier maxJumpHeight { get; private set; }
+        public FloatValueModifier jetpackForce { get; private set; }
+        public FloatValueModifier dashSpeed { get; private set; }
+
+        public Func<bool, bool> GetBoolOverride(BoolData data) { return null; }
+        public Func<int, int> GetIntOverride(IntData data) { return null; }
+
+        public Func<float, float> GetFloatOverride(FloatData data)
+        {
+            switch (data.name)
+            {
+                // Grounded move speed
+                case "moveSpeedWalking":
+                    return moveSpeed.GetModifiedValue;
+                case "moveSpeedWalkAiming":
+                    return moveSpeed.GetModifiedValue;
+                case "moveSpeedSprinting":
+                    return moveSpeed.GetModifiedValue;
+                case "moveSpeedSprintAiming":
+                    return moveSpeed.GetModifiedValue;
+                case "moveSpeedCrouching":
+                    return moveSpeed.GetModifiedValue;
+
+                // Airborne move speed
+                case "moveSpeedAirWalk":
+                    return moveSpeedAirborne.GetModifiedValue;
+                case "moveSpeedAirSprint":
+                    return moveSpeedAirborne.GetModifiedValue;
+                case "moveSpeedAirCrouch":
+                    return moveSpeedAirborne.GetModifiedValue;
+
+                // Acceleration
+                case "acceleration":
+                    return acceleration.GetModifiedValue;
+                case "accelerationAirborne":
+                    return acceleration.GetModifiedValue;
+                case "deceleration":
+                    return acceleration.GetModifiedValue;
+
+                // Other
+                case "maxJumpHeight":
+                    return maxJumpHeight.GetModifiedValue;
+                case "jetpackForce":
+                    return jetpackForce.GetModifiedValue;
+                case "dashSpeed":
+                    return dashSpeed.GetModifiedValue;
+
+                default:
+                    return null;
+            }
+        }
+
+        void InitialiseDataModifiers()
+        {
+            var persistent = RogueLiteManager.persistentData;
+            moveSpeed = new FloatValueModifier(persistent.moveSpeedMultiplier, persistent.moveSpeedPreAdd, persistent.moveSpeedPostAdd); Debug.Log("Move speed multiplier: " + persistent.moveSpeedMultiplier);
+            moveSpeedAirborne = new FloatValueModifier(persistent.airbourneSpeedMultiplier, persistent.airbourneSpeedPreAdd, persistent.airbourneSpeedPostAdd);
+            acceleration = new FloatValueModifier(persistent.accelerationMultiplier, persistent.accelerationdPreAdd, persistent.accelerationPostAdd);
+            maxJumpHeight = new FloatValueModifier(persistent.maxJumpHeightMultiplier, persistent.maxJumpHeightPreAdd, persistent.maxJumpHeightPostAdd);
+            jetpackForce = new FloatValueModifier(persistent.jetpackForceMultiplier, persistent.jetpackForcePreAdd, persistent.jetpackForcePostAdd);
+            dashSpeed = new FloatValueModifier(persistent.dashSpeedMultiplier, persistent.dashSpeedPreAdd, persistent.dashSpeedPostAdd);
+        }
+
+        #endregion
+
+        #region PARAMETERS
+
+        /*
+
+        SWITCH -
+        - canAimHover
+        - canDash
+        - canGrapple
+        - canJetpack
+        - canWallRun
+        - canWallRunUp
+
+        INT -
+        - maxAirJumpCount
+
+        FLOAT -
+        - minDashInterval
+        - minAirJumpInterval
+
+        VECTOR -
+        - crouchDash
+
+        */
+
+        private SwitchParameter m_CanAimHover = null;
+        public bool canAimHover
+        {
+            get { return m_CanAimHover.on; }
+            set { m_CanAimHover.on = value; }
+        }
+
+        private SwitchParameter m_CanDash = null;
+        public bool canDash
+        {
+            get { return m_CanDash.on; }
+            set { m_CanDash.on = value; }
+        }
+
+        private SwitchParameter m_CanGrapple = null;
+        public bool canGrapple
+        {
+            get { return m_CanGrapple.on; }
+            set { m_CanGrapple.on = value; }
+        }
+
+        private SwitchParameter m_CanJetpack = null;
+        public bool canJetpack
+        {
+            get { return m_CanJetpack.on; }
+            set { m_CanJetpack.on = value; }
+        }
 
         private SwitchParameter m_CanWallRun = null;
         public bool canWallRun
@@ -20,45 +160,72 @@ namespace Playground
             set { m_CanWallRun.on = value; }
         }
 
-        void Start()
+        private SwitchParameter m_CanWallRunUp = null;
+        public bool canWallRunUp
         {
-            // Get the persistent player data & motion graph
-            var persistent = RogueLiteManager.persistentData;
-            var motionGraph = GetComponent<MotionController>().motionGraph;
-
-            // Get the required parameters
-            m_CanWallRun = motionGraph.GetSwitchProperty("canWallRun");
-
-            // Set up value modifiers for motion graph data
-            moveSpeed = new FloatValueModifier(persistent.moveSpeedMultiplier, persistent.moveSpeedPreAdd, persistent.moveSpeedPostAdd);
-
-            // Connect up the various data overrides
-            motionGraph.AddDataOverrides(this);
+            get { return m_CanWallRunUp.on; }
+            set { m_CanWallRunUp.on = value; }
         }
 
-        public Func<float, float> GetFloatOverride(FloatData data)
+        private IntParameter m_MaxAirJumpCount = null;
+        public int maxAirJumpCount
         {
-            switch (data.name)
-            {
-                case "moveSpeed":
-                    return moveSpeed.GetModifiedValue;
-                default:
-                    return null;
-            }
+            get { return m_MaxAirJumpCount.value; }
+            set { m_MaxAirJumpCount.value = value; }
         }
 
-        #region IGNORE
-
-        public Func<bool, bool> GetBoolOverride(BoolData data)
+        private FloatParameter m_MinDashInterval = null;
+        public float minDashInterval
         {
-            return null;
+            get { return m_MinDashInterval.value; }
+            set { m_MinDashInterval.value = value; }
         }
 
-        public Func<int, int> GetIntOverride(IntData data)
+        private FloatParameter m_MinAirJumpInterval = null;
+        public float minAirJumpInterval
         {
-            return null;
+            get { return m_MinAirJumpInterval.value; }
+            set { m_MinAirJumpInterval.value = value; }
+        }
+
+        private VectorParameter m_CrouchDash = null;
+        public float crouchDashSpeedBoost
+        {
+            get { return m_CrouchDash.value.z; }
+            set { m_CrouchDash.value = new Vector3(0f, 0f, value); }
+        }
+
+        void InitialiseParameters()
+        {
+            m_CanAimHover = m_MotionGraph.GetSwitchProperty("canAimHover");
+            m_CanDash = m_MotionGraph.GetSwitchProperty("canDash");
+            m_CanGrapple = m_MotionGraph.GetSwitchProperty("canGrapple");
+            m_CanJetpack = m_MotionGraph.GetSwitchProperty("canJetpack");
+            m_CanWallRun = m_MotionGraph.GetSwitchProperty("canWallRun");
+            m_CanWallRunUp = m_MotionGraph.GetSwitchProperty("canWallRunUp");
+            m_MaxAirJumpCount = m_MotionGraph.GetIntProperty("maxAirJumpCount");
+            m_MinDashInterval = m_MotionGraph.GetFloatProperty("minDashInterval");
+            m_MinAirJumpInterval = m_MotionGraph.GetFloatProperty("minAirJumpInterval");
+            m_CrouchDash = m_MotionGraph.GetVectorProperty("crouchDashSpeedBoost");
         }
 
         #endregion
+
+        private MotionGraphContainer m_MotionGraph = null;
+
+        void Start()
+        {
+            // Get the persistent player data & motion graph
+            m_MotionGraph = GetComponent<MotionController>().motionGraph;
+
+            // Get the required parameters
+            InitialiseParameters();
+
+            // Set up value modifiers for motion graph data
+            InitialiseDataModifiers();
+
+            // Connect up the various data overrides
+            m_MotionGraph.AddDataOverrides(this);
+        }
     }
 }
