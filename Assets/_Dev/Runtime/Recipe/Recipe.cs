3bd76a7a42acd29846c03d1e69b514c68def910b
Sorra the Orc <sorra@thewizardscode.com>
2023-12-27 16:29:20 -0800

    UI niceness

--- Assets/_Dev/Runtime/Recipe/Recipe.cs
+++ Assets/_Dev/Runtime/Recipe/Recipe.cs
@@ -12,8 +12,12 @@ namespace Playground
     public class Recipe<T> : ScriptableObject, IRecipe where T : MonoBehaviour
     {
         [Header("Metadata")]
-        [SerializeField, Tooltip("The name of this recipe.")]
+        [SerializeField, Tooltip("The name of this recipe. Used in the UI for the player.")]
         string displayName = "TBD";
+        [SerializeField, TextArea(1,4), Tooltip("A short description of this recipe helping the player understand what it is.")]
+        string description = "TBD";
+        [SerializeField, Tooltip("An image to use as the hero image for this recipe.")]
+        Texture2D heroImage;
         [SerializeField, Tooltip("DO NOT CHANGE THIS. TODO: Create a custom editor that hides this in case of accidental change.")]
         string uniqueID;
         [SerializeField, Tooltip("Powerups are recipes that can be offered between levels and, if purchased, become permanent.")]
@@ -46,6 +50,10 @@ namespace Playground
         public string UniqueID => uniqueID;
 
         public string DisplayName => displayName;
+
+        public string Description => description;
+
+        public Texture2D HeroImage => heroImage;
 
         public bool IsPowerUp => isPowerUp;
 
