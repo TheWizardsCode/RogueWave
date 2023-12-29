0df2ba6e7c6008d3ffd6a547fa31d929392c9913
Sorra the Orc <sorra@thewizardscode.com>
2023-12-28 19:25:54 -0800

    Create a Generic Item Pickup recipe type

---
+++ Assets/_Dev/Runtime/Recipe/GenericItemRecipe.cs
@@ -0,0 +1,20 @@
+﻿using NeoFPS;
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Playground
+{
+    /// <summary>
+    /// Creates a recipe for an item. This is a generic class that can be used to create any item.
+    /// </summary>
+    /// <typeparam name="T">The type of item that will be created.</typeparam>
+    /// <seealso cref="AmmoPickupRecipe"/>
+    /// <seealso cref="WeaponPickupRecipe"/>
+    /// <seealso cref="ToolPickupRecipe"/>
+    [CreateAssetMenu(fileName = "Item Pickup Recipe", menuName = "Playground/Generic Item Pickup Recipe", order = 100)]
+    public class GenericItemRecipe : ItemRecipe<Pickup>
+    {
+    }
+}
