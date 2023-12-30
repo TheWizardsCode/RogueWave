2d5e29abbe05e1bc73e27662e3c872a976b8c2ff
Sorra the Orc <sorra@thewizardscode.com>
2023-12-30 13:39:30 -0800

    Add shield upgrade and a little folder refactoring

---
+++ Assets/_Dev/Runtime/Recipe/ShieldPickupRecipe.cs
@@ -0,0 +1,20 @@
+﻿using NeoFPS;
+using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Playground
+{
+    /// <summary>
+    /// Creates a recipe for an item. This is a generic class that can be used to create any item.
+    /// </summary>
+    /// <typeparam name="T">The type of item that will be created.</typeparam>
+    /// <seealso cref="AmmoPickupRecipe"/>
+    /// <seealso cref="WeaponPickupRecipe"/>
+    /// <seealso cref="ToolPickupRecipe"/>
+    [CreateAssetMenu(fileName = "Shield Pickup Recipe", menuName = "Playground/Shield Pickup Recipe", order = 100)]
+    public class ShieldPickupRecipe : ItemRecipe<ShieldPickup>
+    {
+    }
+}
