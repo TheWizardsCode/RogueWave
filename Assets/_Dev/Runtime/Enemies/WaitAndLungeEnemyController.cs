ed481cf43ff3eaddd9f08418a522a47ac9643f1e
Sorra <sorra@thewizardscode.com>
2024-01-01 02:20:09 -0800

    Use Naughty Attributes to create a better Inspector for enemies.

--- Assets/_Dev/Runtime/Enemies/WaitAndLungeEnemyController.cs
+++ Assets/_Dev/Runtime/Enemies/WaitAndLungeEnemyController.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using NeoFPS.SinglePlayer;
 using UnityEngine;
 
@@ -9,10 +10,9 @@ namespace Playground
     /// </summary>
     public class WaitAndLungeEnemyController : BasicEnemyController
     {
-        [Header("Behaviour")]
-        [SerializeField, Tooltip("Minimum distance to attack from. If the player is further away than this the enemy will not attack. Note that if require line of sight is true and view distance is less than this value then this value will have not effect.")]
+        [SerializeField, Tooltip("Minimum distance to attack from. If the player is further away than this the enemy will not attack. Note that if require line of sight is true and view distance is less than this value then this value will have not effect."), Foldout("Behaviour")]
         protected float minAttackDistance = 20f;
-        [SerializeField, Tooltip("The multiplier for speed when attacking.")]
+        [SerializeField, Tooltip("The multiplier for speed when attacking."), Foldout("Behaviour")]
         protected float attackSpeedMultiplier = 2f;
 
         protected override void Update()
