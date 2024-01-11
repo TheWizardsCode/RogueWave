5111ceca9173c23d121720f182d0b496023e2b32
Sorra <sorra@thewizardscode.com>
2024-01-10 21:01:19 -0800

    add version number to the settings menu

---
+++ Assets/_Dev/Runtime/UI/DeveloperUI.cs
@@ -0,0 +1,24 @@
+﻿using NeoFPS;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using static NeoFPS.HealthDelegates;
+
+namespace Playground
+{
+    public class DeveloperUI : PlayerCharacterHudBase
+    {
+        [SerializeField, Tooltip("The text element to display the version number in.")]
+        TMP_Text versionText;
+
+        private void Awake()
+        {
+            versionText.text = $"v{Application.version}";
+        }
+
+        public override void OnPlayerCharacterChanged(ICharacter character)
+        {
+            
+        }
+    }
+}
