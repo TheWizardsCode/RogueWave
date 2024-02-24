c4f028ed98eb7d2efbf5815a5cf0fe919357ca10
Sorra the Orc <sorra@thewizardscode.com>
2024-02-24 14:17:30 -0800

    prevent a problem that meant the artillery was firing repeatedly when the player was moving at high speeds (e.g. dash)

--- Assets/_Dev/Runtime/Enemies/EnemyFirearmController.cs
+++ Assets/_Dev/Runtime/Enemies/EnemyFirearmController.cs
@@ -1,15 +1,14 @@
-using NeoFPS;
 using NeoFPS.ModularFirearms;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace RogueWave
 {
-    public class EnemyFirearm : MonoBehaviour
+    public class EnemyFirearmController : MonoBehaviour
     {
         [SerializeField, Tooltip("If true the weapon will aim at the player. Make this false for guided projectiles.")]
         private bool _AimAtPlayer = true;
+        [SerializeField, Tooltip("Weapon cooldown time. This is the time that must elapse between presses of the triger. Note that settings on the weapon may restrict actual firing even further.")]
+        private float _cooldown = 2f;
         [SerializeField, Tooltip("Weapon startup time. This is the time that must elapse between creation of the weapon and its first firing.")]
         private float _StartupTime = 3f;
 
@@ -17,6 +16,7 @@ namespace RogueWave
 
         private ModularFirearm m_Firearm = null;
         private bool m_TriggerDown = false;
+        private float m_CooldownRemaining = 0;
 
         private void Start()
         {
@@ -32,22 +32,22 @@ namespace RogueWave
                 return;
             }
 
-            if (!m_TriggerDown)
+            m_CooldownRemaining -= Time.deltaTime;
+
+            if (!m_TriggerDown && m_CooldownRemaining <= 0)
             {
                 if (controller.shouldAttack)
                 {
                     m_TriggerDown = true;
                     m_Firearm.trigger.Press();
+                    m_CooldownRemaining = _cooldown;
                 }
             }
-            else
+            else if (m_TriggerDown)
             {
-                if (!controller.shouldAttack)
-                {
-                    m_TriggerDown = false;
-                    m_Firearm.trigger.Release();
-                    Debug.Log("Release");
-                }
+                m_TriggerDown = false;
+                m_Firearm.trigger.Release();
+                Debug.Log($"{this} release trigger. Target {controller.Target} is at position {controller.Target.position} (RB at {controller.Target.GetComponent<Rigidbody>().position})");
             }
 
             if (_AimAtPlayer && controller.Target != null)
