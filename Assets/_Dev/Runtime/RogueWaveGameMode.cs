17b4fcbd230bae24d704244a5d3776e28d51ad7c
Sorra the Orc <sorra@thewizardscode.com>
2024-02-23 17:05:20 -0800

    Some level design. Fix a bug in which the nanobot level wasn't persisting on succesful runs/

--- Assets/_Dev/Runtime/RogueWaveGameMode.cs
+++ Assets/_Dev/Runtime/RogueWaveGameMode.cs
@@ -120,6 +120,7 @@ namespace RogueWave
         protected override void DelayedDeathAction()
         {
             RogueLiteManager.ResetRunData();
+            RogueLiteManager.persistentData.currentNanobotLevel = 0;
 
             NeoSceneManager.LoadScene(RogueLiteManager.hubScene);
         }
