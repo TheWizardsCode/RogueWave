1cc9b17072c7256bf0bcd363ba315a8491a7f192
Sorra the Orc <sorra@thewizardscode.com>
2024-02-24 18:18:16 -0800

    Break up the level 6 waves so they are not too overpowering. Ensure passive weapons are equipped on startup.

--- Assets/_Dev/Runtime/Recipe/PassiveItemPickupRecipe.cs
+++ Assets/_Dev/Runtime/Recipe/PassiveItemPickupRecipe.cs
@@ -1,4 +1,6 @@
 using NeoFPS;
+using NeoFPS.SinglePlayer;
+using System;
 using UnityEngine;
 
 namespace RogueWave
@@ -6,5 +8,23 @@ namespace RogueWave
     [CreateAssetMenu(fileName = "Passive Item Pickup Recipe", menuName = "Rogue Wave/Recipe/Passive Item Pickup", order = 106)]
     public class PassiveItemPickupRecipe : ItemRecipe<PassivePickup>
     {
+        /// <summary>
+        /// Apply this recipe if there is not too many applied already.
+        /// </summary>
+        /// <param name="manager">The Nanobot Manager applying this recipe.</param>
+        /// <returns>True if succesfully applied, otherwise false.</returns>
+        internal bool Apply(NanobotManager manager)
+        {
+            int applied = manager.GetAppliedCount(this);
+
+            if (applied < MaxStack)
+            {
+                PassiveWeapon passiveWeapon = Instantiate(pickup.itemPrefab);
+                passiveWeapon.transform.SetParent(manager.transform, false);
+                return true;
+            }
+
+            return false;
+        }
     }
 }
