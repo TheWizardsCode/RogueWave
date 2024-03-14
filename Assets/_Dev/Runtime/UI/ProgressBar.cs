cb1ee6acb3cf0d815814d011b30738d07891178d
Sorra the Orc <sorra@thewizardscode.com>
2024-03-14 00:23:10 -0700

    Add a level progress bar with indicators of when waves start.

---
+++ Assets/_Dev/Runtime/UI/ProgressBar.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RogueWave.UI
+{
+    public class ProgressBar : MonoBehaviour
+    {
+        [SerializeField, Tooltip("The minimum value of the bar.")]
+        float minValue = 0;
+        [SerializeField, Tooltip("The maximum value of the bar.")]
+        float maxValue = 100;
+        [SerializeField, Tooltip("The mask that will be used to increase/decrease the progress meter.")]
+        Image mask;
+
+        float value;
+        float fullValue;
+
+        public float MinValue
+        {
+            get => minValue;
+            set
+            {
+                minValue = value;
+                Value = value;
+            }
+        }
+
+        public float MaxValue
+        {
+            get => maxValue;
+            set
+            {
+                maxValue = value;
+                Value = value;
+            }
+        }
+
+        public float Value
+        {
+            get => value;
+            set
+            {
+                if (value < minValue)
+                    value = minValue;
+                else if (value > maxValue)
+                    value = maxValue;
+
+                this.value = value;
+                mask.fillAmount = (value - minValue) / (maxValue - minValue);
+            }
+        }
+    }
+}
