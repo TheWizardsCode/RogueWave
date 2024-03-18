5693a3e1919aad59f725b4026ff3e3348af40cdf
Sorra the Orc <sorra@thewizardscode.com>
2024-03-17 17:50:21 -0700

    Make stats work with or without steam

--- Assets/_Dev/Runtime/Stats/GameStat.cs
+++ Assets/_Dev/Runtime/Stats/GameStat.cs
@@ -1,3 +1,4 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace WizardsCode.GameStats
@@ -16,7 +17,77 @@ namespace WizardsCode.GameStats
         [SerializeField, Tooltip("The type of stat.")]
         StatType m_StatType;
 
+        [ShowNonSerializedField]
+        int m_intValue;
+        [ShowNonSerializedField]
+        float m_floatValue;
+
         public string Key => m_Key;
         public StatType Type => m_StatType;
+
+        internal void Reset()
+        {
+            m_intValue = 0;
+            m_floatValue = 0;
+        }
+
+        public void SetValue(int value)
+        {
+            if (m_StatType == StatType.Float)
+            {
+                Debug.LogWarning("Asking to set an int value on a float stat. This will be cast to a float.");
+                SetValue((float)value);
+            }
+
+            m_intValue = value;
+        }
+
+        public int GetIntValue()
+        {
+            if (m_StatType == StatType.Float)
+            {
+                Debug.LogWarning("Asking for an int value from a float stat. Float has been rounded to an int.");
+                return Mathf.RoundToInt(m_floatValue);
+            }
+            return m_intValue;
+        }
+
+        public void SetValue(float value)
+        {
+            if (m_StatType == StatType.Int)
+            {
+                Debug.LogWarning("Asking to set a float value on an int stat. This will be rounded to an int.");
+                value = Mathf.RoundToInt(value);
+            }
+            m_floatValue = value;
+        }
+
+        public float GetFloatValue()
+        {
+            if (m_StatType == StatType.Int)
+            {
+                Debug.LogWarning("Asking for a float value from an int stat. Int has been cast to a float.");
+                return m_intValue;
+            }
+            return m_floatValue;
+        }
+
+        /// <summary>
+        /// Increments an integer value by a set amount.
+        /// </summary>
+        /// <param name="amount">The amoun to increment the stat.</param>
+        internal void Increment(int amount)
+        {
+            m_intValue += amount;
+        }
+
+        /// <summary>
+        /// Increments an float value by a set amount.
+        /// </summary>
+        /// <param name="amount">The amoun to increment the stat.</param>
+        internal void Increment(float amount)
+        {
+            m_floatValue += amount;
+        }
     }
 }
