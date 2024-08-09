394079197b830354206720c7b7cd155e401fb405
Sorra the Orc <sorra@thewizardscode.com>
2024-08-09 00:54:50 -0700

    Add buttons to wishlist and follow to main menu. Move them to the bottom of the screen.

--- Assets/_Dev/Runtime/UI/UI Controllers/RW_MainMenu.cs
+++ Assets/_Dev/Runtime/UI/UI Controllers/RW_MainMenu.cs
@@ -7,6 +7,10 @@ namespace RogueWave.UI
 {
     public class RW_MainMenu : MainMenu
     {
+        [SerializeField, Tooltip("The URL to open when the Wishlist button is clicked.")]
+        private string wishlistUrl = "https://store.steampowered.com/app/2895630/Rogue_Wave";
+        [SerializeField, Tooltip("The URL to open when the Follow button is clicked.")]
+        private string followUrl = "https://store.steampowered.com/app/2895630/Rogue_Wave";
         [SerializeField, Tooltip("The URL to open when the user clicks the 'Join Discord' button.")]
         private string joinDiscordUrl = "https://discord.gg/Mp6XAz9T6w";
 
@@ -31,6 +35,30 @@ namespace RogueWave.UI
             catch (Exception ex)
             {
                 GameLog.LogError($"User attempted to connect to Discord but got an error: {ex.Message}");
+            }
+        }
+
+        public void Wishlist()
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(wishlistUrl);
+            }
+            catch (Exception ex)
+            {
+                GameLog.LogError($"User attempted to connect to the Wishlist but got an error: {ex.Message}");
+            }
+        }
+
+        public void Follow()
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(followUrl);
+            }
+            catch (Exception ex)
+            {
+                GameLog.LogError($"User attempted to connect to the Follow page but got an error: {ex.Message}");
             }
         }
 
